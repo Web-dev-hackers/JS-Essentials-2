1e258e0054629b84cffce2615e9bbec107cf84e2
Essam Harous <esamfarg91@gmail.com>
2022-01-06 16:27:16 +0200

    (fix) update homework

--- homework.js
+++ homework.js
@@ -1,2 +1,8 @@
 //create a function called 'isScammer' that receives 3 inputs 'isLoggedIn', 'isAuthenticated' and 'isSubscribed'
-//check if one of these parameters has no value return false
+//check if one of these parameters has no value return false otherwise return true
+
+//after that uncomment the next lines
+//var result1 = isScammer('', true, 56)
+//var result2 = isScammer('str', true, 0)
+//var result3 = isScammer('whatever', false, 88)
+//console.log(result1, result2, result3) // all of them false
