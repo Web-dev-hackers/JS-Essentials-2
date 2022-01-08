e87b74a3871ffa2f0a529e7caa2e1d60a4543a7f
taha_tarbah <tahatarbah@gmail.com>
2022-01-08 16:52:18 +0200

    taha

--- homework.js
+++ homework.js
@@ -6,3 +6,32 @@
 //var result2 = isScammer('str', true, 0)
 //var result3 = isScammer('whatever', false, 88)
 //console.log(result1, result2, result3) // all of them false
+//create a function called 'isScammer' that receives 3 inputs 'isLoggedIn', 'isAuthenticated' and 'isSubscribed'
+//check if one of these parameters has no value return false otherwise return true
+
+var isLoggedIn = String
+var isAuthenticated = Boolean
+var isSubscribed = Number
+
+function isScammer(isLoggedIn,isAuthenticated,isSubscribed){
+   
+    if ((isLoggedIn !== true && isAuthenticated !== true && isSubscribed == true)){
+    return "true"
+    }
+    // else if ((isLoggedIn !== true && isAuthenticated == true && isSubscribed !== true)){
+    //     return "true"
+    // }
+    // else if ((isLoggedIn == true && isAuthenticated !== true && isSubscribed !== true)){
+    //         return "true"
+    // }
+    else {
+        return "false"
+    }
+
+}
+
+// after that uncomment the next lines
+var result1 = isScammer('', true, 56)
+var result2 = isScammer('str', true, 0)
+var result3 = isScammer('whatever', false, 88)
+console.log(result1, result2, result3) // all of them false
