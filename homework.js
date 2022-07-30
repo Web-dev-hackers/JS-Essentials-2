be0b4d94d88d06f7085d0e42885619bb5ba3a288
Essam Harous <53625910+Essam-Harrous@users.noreply.github.com>
2022-07-30 20:50:05 +0200

    new home work

--- homework.js
+++ homework.js
@@ -1,3 +1,18 @@
+
+var schoolName;
+var isMarid;
+var job;
+//1. create a function called 'setDetails' that updates the above 3 variables with your details
+//and call it (invoke it)
+
+//2. create a function called 'combineDetails' that receives a contactor (eg: '-', ' ', '_')
+//and return the combination of your details with the 'contactor' param between all of them
+
+//after that uncomment the next 2 lines...
+// var myDetails = combineDetails("-"); //here the contractor value is dash
+// console.log(myDetails); // you should see your full details on the console ;)
+
+
 //create a function called 'isScammer' that receives 3 inputs 'isLoggedIn', 'isAuthenticated' and 'isSubscribed'
 //check if one of these parameters has no value return false otherwise return true
 
